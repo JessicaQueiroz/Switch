a5d6d1a5d011c1f990d3688d746cdbd1ebc9a676
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-11-02 15:59:36 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        switch(2) {
+            case 1:
+                System.out.println("Cartão de crédito selecionado");
+                break;
+            case 2:
+                System.out.println("Saldo da sua conta");
+                break;
+            default:
+                System.out.println("Falar com o atendente");
+        }
+    }
+}
